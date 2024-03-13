3c50f811c85fc4ced0efda383c5adcd08f940059
Your Name <you@example.com>
2024-03-13 13:30:12 +0200

    curs7

---
+++ curs6/js/ap1_1.js
@@ -0,0 +1,12 @@
+export function add(a, b){
+    let suma = a + b;
+    return suma;
+}
+export function dif(a, b){
+    let diferenta = a - b;
+    return diferenta;
+}
+export function inm(a, b){
+    let produs = a * b;
+    return produs;
+}
---
+++ curs6/js/calculator.js
@@ -0,0 +1,6 @@
+import {add} from './ap1_1.js';
+
+let x = 6;
+let y = 4;
+
+console.info( add(x, y));
